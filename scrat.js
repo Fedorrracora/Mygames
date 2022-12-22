5745486b2047c359c03696c8b3ac83e5027a8289
Fedorrracora <93056923+Fedorrracora@users.noreply.github.com>
2022-12-22 20:40:18 +0300

    Update scrat.js

--- scrat.js
+++ scrat.js
@@ -19,20 +19,31 @@ class Audio {
 		}
 	  }
         },
-	{
+		{
           opcode: 'doun',
           blockType: Scratch.BlockType.REPORTER,
           text: 'button [DOWN] [STR]',
-	  arguments: {
-		DOWN: {
-			type: Scratch.ArgumentType.NUMBER,
-			defaultValue: 0
-		},
-		STR: {
-			type: Scratch.ArgumentType.STRING,
-			defaultValue: ''
-		}
-	  }
+			arguments: {
+				DOWN: {
+					type: Scratch.ArgumentType.NUMBER,
+					defaultValue: 0
+				},
+				STR: {
+					type: Scratch.ArgumentType.STRING,
+					defaultValue: ''
+				}
+			}
+        }
+        {
+        opcode: 'tob',
+        blockType: Scratch.BlockType.REPORTER,
+        text: 'button check [NOM]',
+			arguments: {
+				NOM: {
+					type: Scratch.ArgumentType.NUMBER,
+					defaultValue: 0
+				}
+			}
         }
       ]
     };
@@ -62,6 +73,10 @@ class Audio {
 		return 0;
 	}
 	return -1;
+  }
+  tob(args) {
+  	var allbuttons = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'f', 't', 'u', 'v', 'w', 'x', 'y', 'z', '/', '.', ',', ';', "'", ']', '[', '<', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '='];
+  	return allbuttons[Number(args.NOM)];
   }
 }
 Scratch.extensions.register(new Audio());
